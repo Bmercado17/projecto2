3869d03c93db64839f2c39323ab8d2b5c163d1ec
Bmercado17 <askotala@yahoo.com>
2019-02-24 13:43:40 -0800

    this will commit the last commit

--- scripts.js
+++ scripts.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
-    var age = parseInt(prompt ("what is your birth date?"));
+    var age = parseInt(prompt ("type in your age?"));
+   
+    if (age >= 21) {
+        $("#background").show();
+        if (age <= 21) {
+            $("#under21").hide();
+        }
+    } else {
+        $("#under21").show();
+    }
 
-if (age >= 21) {
- $("#background").show();
-} else {
-    $("#under21").show();
-}
+
+    $('#daysvacation').change(function() {
+        image = $(this).val();
+
+        $('.showImage').html('<img src="'+image+'" />');
+    });
+    
 });
+
