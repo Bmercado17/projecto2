2c09e1039667d4e045601772931d8318db8fb043
Bmercado17 <askotala@yahoo.com>
2019-03-06 19:15:30 -0800

    last attempt that will commit

--- scripts.js
+++ scripts.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
-    var age = parseInt(prompt ("type in your age?"));
-   
-    if (age >= 21) {
-        $("#background").show();
-        if (age <= 21) {
-            $("#under21").hide();
-        }
-    } else {
-        $("#under21").show();
-    }
-
-
-    $('#daysvacation').change(function() {
-        image = $(this).val();
-
-        $('.showImage').html('<img src="'+image+'" />');
-    });
-    
+  //     var age = parseInt(prompt("type in your age?"));
+  //     if (age >= 21) {
+  //       $("#background").show();
+  //     } else {
+  //       $("#under21").hide();
+  //     }
+  //     $("#daysvacation").change(function() {
+  //       image = $(this).val();
+  //       $(".showImage").html('<img src="' + image + '" />');
 });
 
+$("#vacation-form").submit(function(event) {
+  var person1Input = $("input#totalpeople").val();
+
+  if (person1Input <= 3) {
+    $("#snorkeling").show();
+  } else if (person1Input > 3 && person1Input <= 7) {
+    $("#cenote").show();
+  } else {
+    $("#quads").show();
+  }
+  event.preventDefault();
+});
